3f11f2d857650e269413c8bc4e5d64b62e601489
Johnathan Falk <johnathan.falk@gmail.com>
2025-04-26 21:18:39 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from vite;
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  path: {
-    aliases: {
-      '/': 'src/index.js'
-    }
+  plugins: [react()],
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
   }
 });
