d817dc90abb0c99ebb4d13938f272075e2fad542
Johnathan Falk <johnathan.falk@gmail.com>
2025-04-26 20:09:46 -0400

    Add vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from \"vite\";
+import { defineConfig } from vite;
 
 export default defineConfig({
   path: {
     aliases: {
-      '/': '/src/index.js'
+      '/': 'src/index.js'
     }
   }
 });
